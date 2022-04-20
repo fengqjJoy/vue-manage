416c6b9a254cd6fd799e947f4f07cb0f9a172c01
fengqj <409902978@qq.com>
2022-04-20 20:15:51 +0800

    表格

--- api/axios.js
+++ api/axios.js
@@ -1,50 +1,55 @@
-import axios from "axios";
-import config from '../config';
-//对axios二次封装
-const baseUrl = process.env.NODE_ENV === 'development' ? config.baseUrl.dev : config.baseUrl.pro;
+// 二次封装axios 拦截器
 
-class HttpRequest {
-    constructor(baseUrl) {
-        this.baseUrl = baseUrl;
+import axios from 'axios'
+import config from '../config/index'
+// 设置配置 根据开发 和 生产环境不一样
+const baseUrl = process.env.NODE_ENV === 'development' ? config.baseUrl.dev : config.baseUrl.pro
+class HttpRequst {
+  constructor(baseUrl) {
+    this.baseUrl = baseUrl
+  }
+  getInsideConfig () {
+    const config = {
+      baseURL: this.baseUrl,
+      header: {
+
+      }
     }
+    return config
+  }
+  interceptors (instance) {
+    instance.interceptors.request.use(function (config) {
+      // 在发送请求之前做些什么
+      return config;
+    }, function (error) {
+      // 对请求错误做些什么
+      return Promise.reject(error);
+    });
 
-    getInsideConfig() {
-        const config = {
-            baseUrl: this.baseUrl,
-            header: {}
-        }
-        return config;
-    }
-
-    interceptors(instance) {
-        // 添加请求拦截器
-        instance.interceptors.request.use(function (config) {
-            // 在发送请求之前做些什么
-            return config;
-        }, function (error) {
-            // 对请求错误做些什么
-            return Promise.reject(error);
-        });
-
-        // 添加响应拦截器
-        instance.interceptors.response.use(function (response) {
-            // 对响应数据做点什么
-            console.log(response)
-            return response;
-        }, function (error) {
-            // 对响应错误做点什么
-            console.log(error, 'error-------')
-            return Promise.reject(error);
-        });
-    }
-
-    request(options) {
-        const instance = axios.create();
-        options = {...this.getInsideConfig(), ...options};//解构
-        this.interceptors(instance);
-        return instance(options);
-    }
-
+    instance.interceptors.response.use(function (response) {
+      console.log('处理响应');
+      // 对响应数据做点什么
+      return response.data;
+    }, function (error) {
+      console.log(error);
+      // 对响应错误做点什么
+      return Promise.reject(error);
+    });
+  }
+  // {
+  //   baseURL:'/rApi'
+  // }
+  request (options) {
+    // 请求
+    // /api/getList  /api/getHome
+    const instanse = axios.create()
+    // 技巧
+    // /api // api1
+    options = { ...(this.getInsideConfig()), ...options }
+    // console.log(options);
+    this.interceptors(instanse)
+    return instanse(options)
+  }
 }
 
-export default new HttpRequest(baseUrl)
+export default new HttpRequst(baseUrl)
--- api/mock.js
+++ api/mock.js
@@ -1,4 +1,17 @@
 import Mock from 'mockjs'
-import homeApi from  './mockServerData/home'
 
-Mock.mock('/home/getData',homeApi.getStatisticalData)
+import homeApi from './mockServerData/home'
+import userApi from './mockServerData/user'
+import permissionApi from './mockServerData/permission'
+Mock.mock('/api/home/getData', homeApi.getStatisticalData)
+// Mock.mock('/api/user/getUser', userApi.getUserList)
+
+
+Mock.mock(/api\/user\/getUser/, 'get', userApi.getUserList)
+// 权限相关
+Mock.mock(/api\/permission\/getMenu/, 'post', permissionApi.getMenu)
+//用户
+Mock.mock(/user\/add/,'post',userApi.createUser);
+Mock.mock(/user\/edit/,'post',userApi.updateUser);
+Mock.mock(/user\/getUser/,'get',userApi.getUserList);
+Mock.mock(/user\/del/,'post',userApi.deleteUser);
